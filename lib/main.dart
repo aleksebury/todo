35f5eed58abb0818409f0d13d7582344e7665968
Buryi Oleksii <aleksebury@gmail.com>
2020-04-18 16:49:55 +0300

    Added amount and dropdown;

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,6 +23,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController taskTitleInputController;
+  TextEditingController taskAmountInputController;
+
+  String _currentType;
+  List<DropdownMenuItem<String>> _dropdownMenuItems;
+
+  @override
+  initState() {
+    super.initState();
+
+    taskTitleInputController = new TextEditingController();
+    taskAmountInputController = new TextEditingController();
+    _dropdownMenuItems = <String>['литры', 'килограммы', 'упаковки', 'бутылки']
+        .map((String value) {
+      return new DropdownMenuItem<String>(
+        value: value,
+        child: new Text(value),
+      );
+    }).toList();
+    _currentType = _dropdownMenuItems[0].value;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,63 +58,67 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  TextEditingController taskTitleInputController;
-// TextEditingController taskDescripInputController;
-
-@override
-initState() {
-  taskTitleInputController = new TextEditingController();
-  // taskDescripInputController = new TextEditingController();
-  super.initState();
-}
-
   _showDialog() async {
-  await showDialog<String>(
-    context: context,
-    child: AlertDialog(
-      contentPadding: const EdgeInsets.all(16.0),
-      content: Column(
-        children: <Widget>[
-          Text("Please fill all fields to create a new item"),
-          Expanded(
-            child: TextField(
-              autofocus: true,
-              decoration: InputDecoration(labelText: 'Item Title*'),
-              controller: taskTitleInputController,
-            ),
-          ),
+    await showDialog<String>(
+      context: context,
+      child: AlertDialog(
+        contentPadding: const EdgeInsets.all(16.0),
+        content: Column(
+          children: <Widget>[
+            Text("Please fill all fields to create a new item"),
+            Column(children: <Widget>[
+              TextField(
+                autofocus: true,
+                decoration: InputDecoration(labelText: 'Item Title*'),
+                controller: taskTitleInputController,
+              ),
+            ]),
+            Row(
+              children: <Widget>[
+                Flexible(
+                    child: TextField(
+                  decoration: new InputDecoration(labelText: "Item amount"),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  controller: taskAmountInputController,
+                )),
+                _buildDropdownButton(context)
+              ],
+            )
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                taskTitleInputController.clear();
+                taskAmountInputController.clear();
+                _currentType = _dropdownMenuItems[0].value;
+                Navigator.pop(context);
+              }),
+          FlatButton(
+              child: Text('Add'),
+              onPressed: () {
+                if (taskTitleInputController.text.isNotEmpty) {
+                  Firestore.instance
+                      .collection('items')
+                      .add({
+                        "item": taskTitleInputController.text,
+                        "amount": taskAmountInputController.text,
+                        "type": _currentType
+                      })
+                      .then((result) => {
+                            Navigator.pop(context),
+                            taskTitleInputController.clear(),
+                            taskAmountInputController.clear(),
+                            _currentType = _dropdownMenuItems[0].value
+                          })
+                      .catchError((err) => print(err));
+                }
+              })
         ],
       ),
-      actions: <Widget>[
-        FlatButton(
-          child: Text('Cancel'),
-          onPressed: () {
-            taskTitleInputController.clear();
-            // taskDescripInputController.clear();
-            Navigator.pop(context);
-          }),
-        FlatButton(
-          child: Text('Add'),
-          onPressed: () {
-            if (taskTitleInputController.text.isNotEmpty) {
-              Firestore.instance
-                .collection('items')
-                .add({
-                  "item": taskTitleInputController.text,
-                  // "description": taskDescripInputController.text
-              })
-              .then((result) => {
-                Navigator.pop(context),
-                taskTitleInputController.clear(),
-                // taskDescripInputController.clear(),
-              })
-              .catchError((err) => print(err));
-          }
-        })
-      ],
-    ),
-  );
-}
+    );
+  }
 
   Widget _buildBody(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
@@ -103,6 +129,17 @@ initState() {
         return _buildList(context, snapshot.data.documents);
       },
     );
+  }
+
+  Widget _buildDropdownButton(BuildContext context) {
+    return DropdownButton<String>(
+        items: _dropdownMenuItems,
+        value: _currentType,
+        onChanged: (val) {
+          setState(() {
+            _currentType = val;
+          });
+        });
   }
 
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
@@ -125,6 +162,7 @@ initState() {
         ),
         child: ListTile(
           title: Text(record.item),
+          trailing: Text(record.amount + " " + record.type),
           onTap: () => record.reference.delete(),
         ),
       ),
@@ -134,16 +172,21 @@ initState() {
 
 class Record {
   final String item;
+  final String amount;
+  final String type;
   final DocumentReference reference;
 
   Record.fromMap(Map<String, dynamic> map, {this.reference})
       : assert(map['item'] != null),
-        item = map['item'];
+        assert(map['amount'] != null),
+        assert(map['type'] != null),
+        item = map['item'],
+        amount = map['amount'],
+        type = map['type'];
 
   Record.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data, reference: snapshot.reference);
 
   @override
-  String toString() => "Record<$item>";
+  String toString() => "Record<$item:$amount $type>";
 }
-
